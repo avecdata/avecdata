e651a2019a097823f0da266379f9d927b1cae3f5
vinicius <viutorres@hotmail.com>
2018-10-02 17:54:00 -0300

    add filters

--- wsgi/myproject/hemocentros/filters.py
+++ wsgi/myproject/hemocentros/filters.py
@@ -1,5 +1,15 @@
 import django_filters
 from hemocentros.models import Hemocentro, v_hemocentro_teste, v_coleta_anual
+from avec.models import pgf_municipio, pgf_entidade, pgf_acao, pgf_acao_detalhe, pgf_acao_faec, pgf_acao_detalhe_faec, pgf_acao_datasus, pgf_acao_datasus_grupo
+
+
+class AcaoFilter(django_filters.FilterSet):
+
+    mes_repasse = django_filters.CharFilter(name='mes', lookup_expr='iexact')
+
+    class Meta:
+        model = pgf_acao
+        fields = ['mes_repasse']
 
 
 class AnoFilter(django_filters.FilterSet):
